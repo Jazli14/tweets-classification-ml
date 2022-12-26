cd4dfb79ca2e58f822f85e82d6bdfd4ab024ef78
Umair-H <93732264+Umair-H@users.noreply.github.com>
2022-12-26 14:21:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ import converter
 if __name__ == "__main__":
     # Fill in bearer token given from twitter API
     token = ""
-    ts.scrapeto("SportsTweets.json", 10000,
-                "AAAAAAAAAAAAAAAAAAAAAKmpkgEAAAAACFo9cY8tDP%2BsUvRS7zbOKpR0zf8"
-                "%3DggxVi0JaBmC7DHc6jI3DWH6OV10OnECBJEyLDTpRmRSwaljXQp")
+    ts.scrapeto("SportsTweets.json", 10000, token)
     sc.classify("SportsTweets.json")
     converter.convert("SportsTweets.json", "SportsTweets.csv")
